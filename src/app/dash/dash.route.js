6db392574b5832874e6e92e83ce3b5de4d9e07c9
Stevo <sdebaun@sparks.network>
2015-09-16 13:22:12 -0700

    uat green plus some animation

--- src/app/dash/dash.route.js
+++ src/app/dash/dash.route.js
@@ -8,7 +8,10 @@ angular
   $stateProvider
   .state('dash', {
     url: '/dash',
-    templateUrl: 'app/dash/dash.html'
+    templateUrl: 'app/dash/dash.html',
+    controller: function($scope, $timeout){
+      $timeout(function(){ $scope.addingCatwalk = 1 }) // in a timeout to make the animation trigger
+    }
   })
 }]);
 
