86f2a586dfb841b3f713f33493f582d4bd3b5dd3
Stevo <sdebaun@sparks.network>
2015-09-16 11:19:09 -0700

    protip: avg can cause intermittent e2e failures
    and those faillures are really frustrating to debug

--- e2e/splash.spec.js
+++ e2e/splash.spec.js
@@ -1,16 +1,13 @@
 'use strict';
 
-ddescribe('Feature 1. Opening app for first time', function () {
+describe('Feature 1. Opening app for first time', function () {
   var page;
 
   beforeEach(function () {
+    var database = require('./firebase');
+    database.clear();
     browser.get('/');
     page = require('./splash.po');
-    // var database = require('./firebase');
-    // database.clear();
-    // browser.waitForAngular();
-    browser.driver.manage().timeouts().implicitlyWait(3000);
-
   });
 
   it('should show a start button', function() {
