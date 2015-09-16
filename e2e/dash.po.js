e990a8fdc92d78dfe8f945af9de69d59f1e57792
Stevo <sdebaun@sparks.network>
2015-09-16 12:00:32 -0700

    uat green and basic styling

--- e2e/dash.po.js
+++ e2e/dash.po.js
@@ -2,7 +2,7 @@
 
 var DashPage = function() {
   this.textHow = element(by.css('input[name=how]'))
-  this.sliderLeashes = element(by.css('input[name=leashes]'))
+  this.sliderLeashes = element(by.css('md-slider.leashes'))
   this.selectFrequency = element(by.css('md-select[name=frequency]'))
   this.timeNext = element(by.css('input[name=next]'))
   this.divOnboarding = element(by.css('div.onboarding'))
